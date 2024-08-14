f71aca89e23cf6f473348c59f325ce5a9dbca9e3
Vinicius Hessel <42287069+Hesselzitos@users.noreply.github.com>
2024-08-13 23:10:28 -0300

    feat: ListQuees and Subscribe

--- python/src/main.py
+++ python/src/main.py
@@ -11,11 +11,13 @@ def run():
     consumerName = "Consumer"
 
     print("Will try to greet world ...")
-    with grpc.insecure_channel("localhost:50051") as channel:
+    with grpc.insecure_channel("localhost:50052") as channel:
         stub = transactionConsumer_pb2_grpc.ConsumerStub(channel)
         listQuees = stub.ListQuees(transactionConsumer_pb2.AskForQuees(ask="Hey, whats quees do you have?"))
-        #response = stub.Subscribe(transactionConsumer_pb2.AskForSubscribe(channel=listQuees[0], consumerName=consumerName))
-    print("Greeter client received: " + listQuees.listQuees)
+        quee=listQuees.listQuees[0]
+        response = stub.Subscribe(transactionConsumer_pb2.AskForSubscribe(channel=quee, consumerName=consumerName))
+    print(response.ack)
+    print(listQuees.listQuees)
 
 
 if __name__ == "__main__":
