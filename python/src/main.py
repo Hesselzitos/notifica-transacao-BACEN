9a5bbed145c77b518b668059ada48f95264a1c57
Vinicius Hessel <42287069+Hesselzitos@users.noreply.github.com>
2024-08-14 00:05:41 -0300

    feat: consume line improved

--- python/src/main.py
+++ python/src/main.py
@@ -14,11 +14,13 @@ def run():
     with grpc.insecure_channel("localhost:50052") as channel:
         stub = transactionConsumer_pb2_grpc.ConsumerStub(channel)
         listQuees = stub.ListQuees(transactionConsumer_pb2.AskForQuees(ask="Hey, whats quees do you have?"))
+        print(listQuees.listQuees)
         quee=listQuees.listQuees[0]
         response = stub.Subscribe(transactionConsumer_pb2.AskForSubscribe(channel=quee, consumerName=consumerName))
-    print(response.ack)
-    print(listQuees.listQuees)
-
+        print(response.ack)
+        if response.ack == "ok":
+            message = stub.ConsumeMessage(transactionConsumer_pb2.AskForMessage(channel=quee, consumerName=consumerName))
+            print(message)
 
 if __name__ == "__main__":
     logging.basicConfig()
